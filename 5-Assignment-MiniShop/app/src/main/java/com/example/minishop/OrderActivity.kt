806f4165f7961e8ac4190430160a00b7af2234bf
GFO <gia.focxverashvili@gmail.com>
2024-11-02 16:30:34 +0400

    add viewBinding in productActivity

--- 5-Assignment-MiniShop/app/src/main/java/com/example/minishop/OrderActivity.kt
+++ 5-Assignment-MiniShop/app/src/main/java/com/example/minishop/OrderActivity.kt
@@ -6,28 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.minishop.databinding.ActivityOrderProductBinding
 
 class OrderActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        lateinit var binding: ActivityOrderProductBinding
+    private lateinit var binding: ActivityOrderProductBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityOrderProductBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val productAmount = binding.productAmount
+
         var count = 0
 
         binding.orderButton.setOnClickListener {
             Intent(this, MainActivity::class.java)
             finish()
         }
-
         binding.plusButton.setOnClickListener {
             count++
-            productAmount.text = count.toString()
+            binding.productAmount.text = count.toString()
         }
 
         binding.minusButton.setOnClickListener {
             count--
-            productAmount.text = count.toString()
+            binding.productAmount.text = count.toString()
         }
 
         binding.backButton.setOnClickListener {
